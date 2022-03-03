ebb51824b2bc701cc70b20b16797e7beac4f8be7
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-03-03 23:00:45 +0530

    Initial commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,15 @@
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c;
+        for(int i=0;i<7;i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+
+}
